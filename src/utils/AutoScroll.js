b022dc6e412199dcc1083560168bff842561879e
cowboy bebop <1164566601@qq.com>
2020-05-08 21:38:26 +0800

    scrollList

---
+++ src/utils/AutoScroll.js
@@ -0,0 +1,42 @@
+export function AutoScrollList(id) {
+  const listBox = document.getElementById(id)
+  let intervalScroll
+  let setTime1
+  let setTime2
+
+  //   stopScroll()
+
+  function startScroll() { // 开始滚动
+    stopScroll()
+    if (listBox.offsetHeight < listBox.scrollHeight) {
+      intervalScroll = setInterval(function() {
+        if ((listBox.scrollTop + listBox.offsetHeight) < listBox.scrollHeight) {
+          listBox.scrollTop++
+        } else {
+          clearInterval(intervalScroll)
+          setTime1 = setTimeout(function() {
+            // $(listBox).animate({scrollTop: 0}, 'fast');
+            listBox.scrollTop = 0
+          }, 3000)
+          setTime2 = setTimeout(startScroll, 5000)
+        }
+      }, 30)
+    }
+  }
+
+  function stopScroll() { // 停止滚动
+    clearInterval(intervalScroll)
+    clearTimeout(setTime1)
+    clearTimeout(setTime2)
+  }
+
+  listBox.addEventListener('mouseover', function() {
+    stopScroll()
+  })
+
+  listBox.addEventListener('mouseout', function() {
+    startScroll()
+  })
+
+  startScroll()
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,68 @@
     1231
     <Scroll />
     <ScrollList />
+    <div id="test" class="noScroll" style="height:150px;overflow: auto;">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+      </ul>
+    </div>
+    <div id="test2" class="noScroll" style="height:150px;overflow: auto;">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,11 +73,16 @@
 // @ is an alias to /src
 import Scroll from '@/components/NoticeList'
 import ScrollList from '@/components/ScrollList'
+import { AutoScrollList } from '@/utils/AutoScroll'
 export default {
   name: 'Home',
   components: {
     Scroll,
     ScrollList
+  },
+  mounted() {
+    AutoScrollList('test')
+    AutoScrollList('test2')
   }
 }
 </script>
