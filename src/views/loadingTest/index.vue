6a2c82152b63b6ce927473f8270a33cec9d67ec8
chenjinhong <1164566601@qq.com>
2020-07-31 21:33:45 +0800

    更换loading

--- src/views/loadingTest/index.vue
+++ src/views/loadingTest/index.vue
@@ -13,6 +13,7 @@
     <div style="width:500px;height:100px">
       <test2 :dataList="dataList" />
     </div>
+    <div class="tick" data-value="Hello World"></div>
   </div>
 </template>
 
@@ -25,6 +26,7 @@ import diydatepicker from './DiydatePicker'
 import ScrollList from '@/components/ScrollList'
 import test from '@/components/NoticeList/test'
 import test2 from '@/components/NoticeList/test2'
+import Tick from '@pqina/flip'
 export default {
   name: 'Loading',
   directives: {
@@ -45,22 +47,14 @@ export default {
       msg: '123',
       loading: true,
       dataList: [
-        { title: '1.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:11' },
-        { title: '2.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:12' },
-        { title: '3.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:13' },
-        { title: '4.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:14' },
-        { title: '5.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:15' },
-        { title: '6.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:16' },
-        { title: '7.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:17' },
-        { title: '8.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:18' },
-        { title: '9.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:19' },
-        { title: '10.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:20' },
-        { title: '11.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:21' }
+        { title: '1.瑞金新村小区新增一条保修工单,瑞金新村小撒旦撒', time: '2020/12/04 19:36:11' }
       ]
     }
   },
   created() {
     this.getMsg()
+    console.log(Tick)
+    Tick()
   },
   methods: {
     getMsg() {
